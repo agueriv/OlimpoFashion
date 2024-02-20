6c541af6f44e6589fb3aab8bf5fad579f2cc211a
agueriv <rivasguerreroariel@gmail.com>
2024-02-20 17:14:31 +0100

    Creación de rutas de recursos para articulos y categorias

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Http\Controllers\ArticuloController;
+use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/', [HomeController::class, 'home'])->name('home');
 Route::get('/almacen', [HomeController::class, 'almacen'])->name('almacen');
+
+Route::resource('almacen/articulo', ArticuloController::class);
+Route::resource('almacen/categoria', CategoriaController::class);
