f50e9f093859736f372c5fa47410302eac09b63b
agueriv <rivasguerreroariel@gmail.com>
2024-02-20 08:32:22 +0100

    Modificación de migración y campo foto

--- database/migrations/2024_02_19_221605_create_articulos_table.php
+++ database/migrations/2024_02_19_221605_create_articulos_table.php
@@ -13,19 +13,33 @@ return new class extends Migration
     {
         Schema::create('articulo', function (Blueprint $table) {
             $table->id();
+            // nombre
             $table->string('nombre', 120);
+            // seccion enum
             $table->enum('seccion', ['h', 'm', 'n', 'all'])->default('all');
+            // temporada del articulo
             $table->enum('temporada', ['pri-ver', 'oto-inv', 'all'])->default('all');
+            // foto del articulo, se podrian convertir en fotos
             $table->binary('picture');
+            // id de la categoria
             $table->foreignId('idcategoria');
             // Definimos la clave foránea
             $table->foreign('idcategoria')->references('id')->on('categoria')->onUpdate('cascade')->onDelete('cascade');
+            // esta rebajado o no
             $table->boolean('en_rebaja')->default(false);
+            // precio del articulo
             $table->decimal('precio', 8, 2);
+            // precio rebajado que puede ser null
             $table->decimal('precio_rebaja', 8, 2)->nullable();
+            // descripcion del producto
             $table->string('descripcion', 1000);
             $table->timestamps();
         });
+
+        // Vamos a cambiar el tipo del campo cover para subir fotos mas grandes
+        $sql = 'alter table articulo change picture picture longblob';
+        //las migraciones de Laravel no ofrecen el tipo longblob
+        DB::statement($sql);
     }
 
     /**
