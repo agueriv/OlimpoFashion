11957c48b2499c1488209fe13b37d0c07be0d457
agueriv <rivasguerreroariel@gmail.com>
2024-02-27 13:18:34 +0100

    Creación de rutas de recursos para admin

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ArticuloController;
 use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\HomeController;
@@ -8,6 +9,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'home'])->name('home');
 Route::get('/almacen', [HomeController::class, 'almacen'])->name('almacen');
 
+Route::resource('almacen/admin', AdminController::class);
 Route::resource('almacen/articulo', ArticuloController::class);
 Route::resource('almacen/categoria', CategoriaController::class, [
     'parameters' => ['categoria' => 'categoria']
