fc7fc6cf2351365a73d7e58196500bbaedb72a83
agueriv <rivasguerreroariel@gmail.com>
2024-02-28 18:20:32 +0100

    Cambios de redireccionamiento en rutas de usuario

--- app/Http/Controllers/Auth/ConfirmPasswordController.php
+++ app/Http/Controllers/Auth/ConfirmPasswordController.php
@@ -25,7 +25,7 @@ class ConfirmPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/almacen';
 
     /**
      * Create a new controller instance.
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/almacen/login';
 
     /**
      * Create a new controller instance.
