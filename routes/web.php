ab094bae16a167c96da69d0a77ac1bde86e49983
agueriv <rivasguerreroariel@gmail.com>
2024-02-28 13:59:16 +0100

    Implementación de rutas de usuarios

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,20 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ArticuloController;
 use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LandingController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'home'])->name('home');
-Route::get('/almacen', [HomeController::class, 'almacen'])->name('almacen');
+Route::get('/', [LandingController::class, 'home'])->name('front.home');
+Route::get('/almacen', [LandingController::class, 'almacen'])->name('almacen');
 
 Route::resource('almacen/admin', AdminController::class);
 Route::resource('almacen/articulo', ArticuloController::class);
 Route::resource('almacen/categoria', CategoriaController::class, [
     'parameters' => ['categoria' => 'categoria']
 ]);
+
+Route::prefix('almacen')->group(function () {
+    Auth::routes(['verify' => true]);
+});
+
+Route::get('almacen/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
