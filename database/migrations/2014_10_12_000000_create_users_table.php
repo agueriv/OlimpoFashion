df3ca75d483edeb6fade2c69a538dd305bf5c487
agueriv <rivasguerreroariel@gmail.com>
2024-03-03 01:28:30 +0100

    Creación de usuarios por defecto

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -21,6 +21,14 @@ return new class extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+
+        $pass = Hash::make('31102004');
+        $sql = "insert into users (name, email, email_verified_at, password, puesto, created_at, updated_at) VALUES ('Ariel', 'ariel@gmail.com', '2024-02-21 10:07:58', '" . $pass . "', 'jefe', '2024-02-21 10:07:58', '2024-02-21 10:07:58');";
+        DB::statement($sql);
+
+        $pass2 = Hash::make('12345678');
+        $sql2 = "insert into users (name, email, email_verified_at, password, puesto, created_at, updated_at) VALUES ('Empleado', 'empleado@gmail.com', '2024-02-21 10:07:58', '" . $pass2 . "', 'empleado', '2024-02-21 10:07:59', '2024-02-21 10:07:59');";
+        DB::statement($sql2);
     }
 
     /**
